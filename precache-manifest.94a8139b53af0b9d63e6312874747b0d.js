7f3a43f04f8369a9f4494d01c26ee92c3466bf11
Alistair Willis <alistairrwillis@gmail.com>
2020-07-31 16:21:36 +0100

    Updates

---
+++ precache-manifest.94a8139b53af0b9d63e6312874747b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2599fd9c02f55412ef6a1ccf742c84a2",
+    "url": "/react-noughts-and-crosses/index.html"
+  },
+  {
+    "revision": "34aa95129520cc9a1ec0",
+    "url": "/react-noughts-and-crosses/static/css/main.c1e4da06.chunk.css"
+  },
+  {
+    "revision": "64790826582aa76adf8a",
+    "url": "/react-noughts-and-crosses/static/js/2.184795c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-noughts-and-crosses/static/js/2.184795c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34aa95129520cc9a1ec0",
+    "url": "/react-noughts-and-crosses/static/js/main.6c5f8935.chunk.js"
+  },
+  {
+    "revision": "b601642b9d8ab52fe944",
+    "url": "/react-noughts-and-crosses/static/js/runtime-main.8600a3fa.js"
+  },
+  {
+    "revision": "5eb70df4e07f63a7da0dd872c52ce7ef",
+    "url": "/react-noughts-and-crosses/static/media/black-bg.5eb70df4.jpg"
+  }
+]);
